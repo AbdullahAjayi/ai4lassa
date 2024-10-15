e009e3db0e5385fff8a4f27b46395ef601c25fad
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-10-15 17:53:38 +0100

    Updated Resources Section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Forecast from "@/components/Forecast";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Report from "@/components/Report";
+import Resources from "@/components/Resources";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Forecast />
       <Report />
+      <Resources />
     </>
 
   );
