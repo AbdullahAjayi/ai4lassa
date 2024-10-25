76f09000266fcb46c1ce66bd8910e5fa579dac22
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-10-25 12:21:49 +0100

    added necessary images to spice up About section

--- components/About.tsx
+++ components/About.tsx
@@ -3,15 +3,18 @@ import Section from "./Section"
 
 const About = () => {
     return (
-        <Section className="pt-20">
+        <Section className="pt-20 relative">
             <h2 className="h2 relative">
                 <span>How Does Our System Help?</span>
                 <div className="absolute top-0 -left-4 -translate-y-2/3">
                     <Image src='/assets/images/rat.png' alt="rat-image" width={134} height={134} className="" />
                 </div>
             </h2>
-            <div className="grid md:grid-cols-2 gap-y-20 md:gap-y-32 mt-16 md:mt-24 md:px-10">
-                <div className="col-start-1">
+            <div className="absolute -z-10 inset-0 top-36 overflow-hidden">
+                <Image src='/assets/images/dot-patterns.png' alt="dot-patterns" width={1832} height={838} />
+            </div>
+            <div className="grid md:grid-cols-2 gap-y-28 md:gap-y-32 mt-16 md:mt-24 md:px-10 mb-10">
+                <div className="col-start-1 relative">
                     <h3 className="h3 relative w-fit">
                         <span>Outbreak Forcasting</span>
                         <div className="absolute -top-16 -right-20">
@@ -19,24 +22,48 @@ const About = () => {
                         </div>
                     </h3>
                     <p className="">Our advanced algorithms predict potential LASSA fever outbreaks based on real-time data, allowing communities to prepare</p>
+                    {/* map vector image */}
+                    <div className="absolute -bottom-16 left-[35%] -translate-x-1/2">
+                        <Image className="scale-[.6]" src='/assets/images/map-vector-1.png' alt='map-vector-image' width={121} height={121} />
+                    </div>
                 </div>
-                <div className="md:col-start-2 row-start-2">
+                <div className="md:col-start-2 row-start-2 relative">
                     <h3 className="h3 relative w-fit">
                         <span>Case Reporting</span>
+                        <div className="max-md:hidden absolute -top-[8.4rem] -left-10">
+                            <Image src='/assets/images/arrow-vector-1.png' alt="arrow-image" width={137} height={181} />
+                        </div>
                         <div className="absolute -top-16 -right-20">
                             <Image src='/assets/images/lassaGreen.png' alt="lassa-image" width={153} height={153} className="opacity-60 -z-10 scale-[.6]" />
                         </div>
                     </h3>
                     <p className="">Suspect a case? Easily report it to health officials through our secure platform. Quick action can save lives</p>
+                    {/* map vector image */}
+                    <div className="absolute -bottom-16 left-0">
+                        <Image className="scale-[.6]" src='/assets/images/map-vector-2a.png' alt='map-vector-image' width={107} height={114} />
+                    </div>
+                    <div className="absolute top-10 right-4">
+                        <Image className="scale-[.6]" src='/assets/images/map-vector-2b.png' alt='map-vector-image' width={74} height={41} />
+                    </div>
                 </div>
-                <div className="col-start-1 row-start-3">
+                <div className="col-start-1 row-start-3 relative">
                     <h3 className="h3 relative w-fit">
                         <span>Real-time Alerts</span>
+                        <div className="max-md:hidden absolute -top-[9rem] -right-24">
+                            <Image src='/assets/images/arrow-vector-2.png' alt="arrow-image" width={123.85} height={217.06} />
+                        </div>
                         <div className="absolute -top-16 -right-20">
                             <Image src='/assets/images/lassaGreen.png' alt="lassa-image" width={153} height={153} className="opacity-60 -z-10 scale-[.6]" />
                         </div>
                     </h3>
                     <p className="">Receive immediate alerts on outbreaks in your area to stay safe and informed</p>
+                    {/* map vector image */}
+                    <div className="absolute -bottom-16 -left-8">
+                        <Image className="scale-[.6]" src='/assets/images/map-vector-3a.png' alt='map-vector-image' width={107} height={114} />
+                    </div>
+                    <div className="absolute top-10 right-10">
+                        <Image className="scale-[.6]" src='/assets/images/map-vector-3b.png' alt='map-vector-image' width={74} height={41} />
+                    </div>
                 </div>
             </div>
         </Section>
