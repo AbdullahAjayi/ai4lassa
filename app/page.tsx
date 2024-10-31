fec44c5d511106d4b6066d90a3c79a73cf4eb830
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-10-31 14:21:34 +0100

    added Loader component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect, useState } from "react"
 import About from "@/components/About";
 import Footer from "@/components/Footer";
 import Forecast from "@/components/Forecast";
@@ -5,8 +8,26 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Report from "@/components/Report";
 import Resources from "@/components/Resources";
+import Loader from "@/components/Loader";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const loadDomContent = async () => {
+      await Promise.all([document.addEventListener('DOMContentLoaded', () => { })])
+      setIsLoading(false)
+    }
+
+    loadDomContent()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <Loader />
+    )
+  }
+
   return (
     <>
       <Header />
@@ -17,6 +38,5 @@ export default function Home() {
       <Resources />
       <Footer />
     </>
-
-  );
+  )
 }
---
+++ components/Loader.tsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+    return (
+        <div className="fixed inset-0 flex justify-center items-center">Loading...</div>
+    )
+}
+
+export default Loader
