5db61b7a4fef241b5b8a3176112e4075ed94a7be
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-10-31 12:35:18 +0100

    added animation to Resources.tsx

--- components/Resources.tsx
+++ components/Resources.tsx
@@ -1,19 +1,25 @@
+"use client"
+
 import Image from "next/image"
 
 import Section from "./Section"
+import { useCharacterAnimation, useSlideUpAnimation } from "@/hooks/useAnimation"
 
 const Resources = () => {
+    useSlideUpAnimation()
+    useCharacterAnimation()
+
     return (
         <Section id="resources" className="pt-20 bg-black/10 min-h-screen flex items-center">
             <div>
                 <h2 className="h2 relative">
-                    <span>Why Curbing LASSA Fever Is Critical</span>
+                    <span className="animate-character">Why Curbing LASSA Fever Is Critical</span>
                     {/* rat image */}
                     <div className="absolute top-0 -left-4 -translate-y-2/3">
                         <Image src='/assets/images/rat.png' alt="rat-image" width={134} height={134} className="" />
                     </div>
                 </h2>
-                <p className="mt-10 md:mt-20 mb-10">Lassa fever, a viral hemorrhagic illness spread by rodents, has devastating effects on affected communities in Nigeria and West Africa. Outbreaks often lead to loss of lives, overwhelming healthcare systems, and economic disruption, particularly in rural areas where healthcare access is limited. Early detection and prompt reporting of suspected cases are critical to containing the spread of the disease. By quickly identifying infected individuals, healthcare workers can initiate timely treatment, and communities can take preventive measures to protect others. With the right tools, such as real-time reporting platforms and educational infographics on hygiene practices, we can mitigate the impact of Lassa fever and save lives.</p>
+                <p className="mt-10 md:mt-20 mb-10 animate-slide-up">Lassa fever, a viral hemorrhagic illness spread by rodents, has devastating effects on affected communities in Nigeria and West Africa. Outbreaks often lead to loss of lives, overwhelming healthcare systems, and economic disruption, particularly in rural areas where healthcare access is limited. Early detection and prompt reporting of suspected cases are critical to containing the spread of the disease. By quickly identifying infected individuals, healthcare workers can initiate timely treatment, and communities can take preventive measures to protect others. With the right tools, such as real-time reporting platforms and educational infographics on hygiene practices, we can mitigate the impact of Lassa fever and save lives.</p>
 
             </div>
         </Section>
