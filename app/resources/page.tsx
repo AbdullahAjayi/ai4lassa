ea5253cd23ed0f9315e59750fb5edae0414eee6c
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-10 10:26:10 +0100

    updated Resources page

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import Image from 'next/image'
+
+import Section from '@/components/Section'
 
 const page = () => {
     return (
-        <div className='min-h-screen text-4xl'>This feature is under development <br /> Our team is working hard to make it happen</div>
+        <Section className='pt-16 min-h-screen flex justify-center items-center'>
+            <div>
+                <h1 className='h1 text-center'>Resources</h1>
+                <Image
+                    src="/assets/svg/coming-soon.svg"
+                    alt="Coming Soon"
+                    width={300}
+                    height={0}
+                    priority
+                    className="rounded-lg mx-auto w-[600px] h-auto"
+                />
+                <p className="text-lg text-center mt-6">Nothing here yet, check again later!</p>
+
+            </div>
+        </Section>
     )
 }
 
