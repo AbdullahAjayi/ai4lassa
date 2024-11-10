55f20c98992dbefc6bf5289eb18d54f81119a75d
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-10 03:13:51 +0100

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,4 @@
-"use client"
-
-import { useCharacterAnimation, useSlideUpAnimation } from "@/hooks/useAnimation"
-
 const Footer = () => {
-    useCharacterAnimation({ stagger: 0.02 })
-    useCharacterAnimation({ selector: '.animate-character-left', x: -30, stagger: 0.02 })
-    useSlideUpAnimation()
-
     return (
         <footer id="contact" className="max-w-[1500px] mx-auto">
             <div className="border-t border-black py-6 px-10 flex justify-center items-center font-semibold">
