a92d9ab4c494837284d333a34799854bf64e9449
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-10 03:35:56 +0100

    updated resources section in home page

--- components/Resources.tsx
+++ components/Resources.tsx
@@ -23,13 +23,12 @@ const Resources = () => {
                     </div>
                 </h2>
                 <p className="mt-10 md:mt-20 mb-10 animate-slide-up">Lassa fever, a viral hemorrhagic illness spread by rodents, has devastating effects on affected communities in Nigeria and West Africa. Outbreaks often lead to loss of lives, overwhelming healthcare systems, and economic disruption, particularly in rural areas where healthcare access is limited. Early detection and prompt reporting of suspected cases are critical to containing the spread of the disease. By quickly identifying infected individuals, healthcare workers can initiate timely treatment, and communities can take preventive measures to protect others. With the right tools, such as real-time reporting platforms and educational infographics on hygiene practices, we can mitigate the impact of Lassa fever and save lives.</p>
-                <Link href='/contact'>
-                    <Button className="mt-6 animate-character">
+                <Link className="mt-6 inline-block" href='/contact'>
+                    <Button className="animate-character flex items-center">
                         <span className="mr-3">Reach out to us</span>
                         <FaLocationArrow />
                     </Button>
                 </Link>
-
             </div>
         </Section>
     )
