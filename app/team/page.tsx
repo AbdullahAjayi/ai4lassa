3d3a206864719ec4fa0727c6e3f02fd46acabbf1
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-10 03:36:57 +0100

    added team page

---
+++ app/team/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='min-h-screen text-4xl'>This feature is under development <br /> Our team is working hard to make it happen</div>
+    )
+}
+
+export default page
