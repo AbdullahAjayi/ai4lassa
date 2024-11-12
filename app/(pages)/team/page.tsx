738e8c210954ef293e934f51489dfae7c0970a59
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-12 14:35:15 +0100

    added new team member data

--- app/(pages)/team/page.tsx
+++ app/(pages)/team/page.tsx
@@ -1,64 +1,10 @@
 import Section from "@/components/Section";
 import Image from "next/image";
 
+import { teamMembers } from "@/data/data";
+
 const page = () => {
-    const teamMembers = [
-        {
-            name: "UMARU, Emmanuel Tanko",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture1.png",
-            bio: "Associate Professor | Researcher | Deputy Director @ WASCAL.",
-            details: ""
-        },
-        {
-            name: "CHINGLE, MOSES PETER",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture2.png",
-            bio: "Professor of Epidemiology and Primary Health Care, University of Jos"
-        },
-        {
-            name: "James Garba Ambafi (PhD)",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture3.png",
-            bio: "Senior Lecturer in the Department of Electrical & Electronics Engineering at the Federal University of Technology, Minna."
-        },
-        {
-            name: "Dr. Rakiya Gambo Musa",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture4.png",
-            bio: "Senior Medical Officer at the University Clinic of the Federal University of Technology, Minna."
-        },
-        {
-            name: "GBENGA MORENIKEJI",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture5.png",
-            bio: "Academic staff member in the Department of Estate Management at the Federal University of Technology, Minna."
-        },
-        {
-            name: "Adeleye Bamiji Michael (PhD)",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture6.png",
-            bio: "Lecturer in the Department of Urban and Regional Planning at the Federal University of Technology Minna."
-        },
-        {
-            name: "Henry Ohiani Ohize (PhD)",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture7.png",
-            bio: "Associate Professor of Electrical and Electronics Engineering at the Federal University of Technology Minna."
-        },
-        {
-            name: "Chibueze Valentine Ikpo (PhD)",
-            role: "Founder & CEO",
-            image: "/assets/images/profiles/Picture8.png",
-            bio: "Lecturer in Software Engineering at Veritas University, Nigeria."
-        },
-        {
-            name: "John Doe",
-            role: "Founder & CEO",
-            image: "/assets/images/placeholder/profile-pic-avatar.avif",
-            bio: "15+ years of experience in software development"
-        },
-    ];
+
 
     return (
         <Section className="min-h-screen py-16 pt-24">
