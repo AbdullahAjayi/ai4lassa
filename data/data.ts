1744e171023518b1d85e9d2c8655a75200f43d47
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-12 18:01:53 +0100

    updated team members data

--- data/data.ts
+++ data/data.ts
@@ -1,65 +1,80 @@
+export interface TeamMemberData {
+  name: string
+  role: string
+  image: string
+  bio: string
+  about: string
+}
+
 export const teamMembers = [
   {
     name: "UMARU, Emmanuel Tanko",
-    role: "Founder & CEO",
+    role: "Principal Researcher/Investigator",
     image: "/assets/images/profiles/Picture1.png",
     bio: "Associate Professor | Researcher | Deputy Director @ WASCAL.",
-    details: "",
+    about:
+      "UMARU, Emmanuel Tanko received his PhD in Urban and Regional Planning (Urban Health) from the Universiti Teknologi Malaysia in 2015. He is an Associate Professor of Urban Planning in the Department of Urban and Regional Planning, School of Environmental Technology, Federal University of Technology Minna, Nigeria. He is also the Deputy Director of the West African Science Service Centre on Climate Change and Adapted Land Use (WASCAL) Federal University of Technology Minna, Niger State, Nigeria. His research interests include Urban Health, Urban Modelling, Climate Change and Migration, and IOT in Climate Science. He is registered with the Nigerian Institute of Town Planners (NITP) and the Town Planners Registration Council (TOPREC). He is presently supervising and co-supervising PhD and Masters students. In addition, he has supervised over 90 final-year research projects to completion. He has published over 35 articles in international and national journals and conference proceedings. Dr. E. T. Umaru's publications have been cited about 155 times (Google Scholar database); he has an author h-index of 7 (Google Scholar database) and h-index of 2 in the Scopus database with 23 citations from 2012 to 2023. Dr. Umaru currently serves as a reviewer for many journals and conferences. He has reviewed over 20 manuscripts for various journals. In addition, he has attended several Conferences.",
   },
   {
     name: "CHINGLE, MOSES PETER",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture2.png",
     bio: "Professor of Epidemiology and Primary Health Care, University of Jos",
-    details: "",
+    about: ` CHINGLE, MOSES PETER (MBBES; MPH; FMCPH)
+            I am a Professor of Epidemiology and Primary Health Care at the University of Jos and an Honorary Consultant with Jos University Teaching Hospital (JUTH). My clinical and research interests include Epidemiology, Primary Health Care, and Health Systems Strengthening. I coordinate the collaborative program between the department and contingent Primary Health Care (PHC) centers, adopted for training and implementation activities by the National Primary Healthcare Development Agency (NPHCDA), with a special emphasis on Maternal, Newborn, and Child Health Care services and Nutrition. Additionally, I am a master trainer for the Community Health Influencers Promoters and Services (CHIPS) program implemented by the NPHCDA in communities and PHC centers.
+            I am an experienced and accomplished Public Health practitioner with over two decades of experience in the medical field. I am skilled in program management, qualitative and quantitative research methods, and advanced report writing across various sectors. I possess extensive knowledge of data collection and analysis tools and techniques, as well as expertise in ICT4D and Microsoft Office tools.
+            I have published over sixty papers in peer reviewed journals and I have served as an undergraduate and postgraduate external examiner at various universities across the country as well as being a postgraduate examiner at the National Postgraduate Medical College of Nigeria (NPMCN). I have also served the NPMCN as member of accreditation team to various teaching hospitals.
+            I have served as a consultant on epidemic control, research and conducted various assessments for Federal Ministry of Health, State Ministries of Health, National Agency for the control of AIDS, UNICEF, FHI 360, CHAI, the Carter Centre, etc.`,
   },
   {
     name: "James Garba Ambafi (PhD)",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture3.png",
     bio: "Senior Lecturer in the Department of Electrical & Electronics Engineering at the Federal University of Technology, Minna.",
-    details: "",
+    about: `James Garba Ambafi, Obtained BEng in Electrical and Electronics Engineering from the Federal University of Technology Yola, Nigeria, an MEng degree in Electrical Power and Machines from the Federal University of Technology Minna, Nigeria, and a Ph.D. degree in Electrical Engineering at Bayero University, Kano, Nigeria. He is currently a Senior Lecturer in the Department of Electrical & Electronics Engineering at the Federal University of Technology, Minna. Nigeria. His research interests include; power system stability and control, hybrid renewable energy systems, computational intelligence. He can be contacted at email: ambafi@futminna.edu.ng`,
   },
   {
     name: "Dr. Rakiya Gambo Musa",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture4.png",
     bio: "Senior Medical Officer at the University Clinic of the Federal University of Technology, Minna.",
-    details: "",
+    about: `Dr. Rakiya Gambo Musa earned her Bachelor of Medicine, Bachelor of Surgery (MBBS) degree from Bayero University, Kano, in 2011 and is currently advancing her expertise through a Master’s in Public Health at Ahmadu Bello University via its Distance Learning Center. Demonstrating a commitment to professional development, she completed a Public Health Internship with HACBY in 2021 and earned a Certificate in Leadership and Management from the University of Washington.
+            With extensive experience in both clinical and fieldwork settings, Dr. Musa is a dedicated public health advocate who combines her clinical expertise with a strong commitment to advancing public health initiatives aimed at improving healthcare outcomes and community wellness.
+            In her current role as Senior Medical Officer at the University Clinic of the Federal University of Technology, Minna, Dr. Musa oversees clinical operations and contributes significantly to the health and wellness of the university community. Her multifaceted experience highlights her dedication to both patient care and the promotion of public health.`,
   },
   {
     name: "GBENGA MORENIKEJI",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture5.png",
     bio: "Academic staff member in the Department of Estate Management at the Federal University of Technology, Minna.",
-    details: "",
+    about: `Gbenga Morenikeji is an academic staff member in the Department of Estate Management at the Federal University of Technology, Minna, and co-coordinator of the Research and Development unit at the Centre for Disaster Risk Management. He also serves as Program Manager at the Institute for Land and Community Resilience and is a Fellow of the Disaster Resilience Leadership Academy, the Emergency, Crisis & Disaster Risk Management Institute, and the Institute of Global Facilities Management. An expert in community-based disaster preparedness, Gbenga has contributed as a UNEP webinar panelist and member of the UNISDR/UNEP/OCHA drafting group for the Sendai Framework. He serves as Africa Region Focal Point for UNEP/APELL and is a member of NELGRA and NELGA, with numerous grants and publications.`,
   },
   {
     name: "Adeleye Bamiji Michael (PhD)",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture6.png",
     bio: "Lecturer in the Department of Urban and Regional Planning at the Federal University of Technology Minna.",
-    details: "",
+    about: `Adeleye Bamiji Michael is a lecturer in the Department of Urban and Regional Planning at the Federal University of Technology Minna. Adeleye Bamiji Michael holds a PhD in Urban and Regional Planning from Makerere University, Kampala, Uganda. Adeleye has published in various reputable peer-reviewed Journals. Adeleye is a Member of the Nigeria Institute of Town Planners (NITP) and the International Association for Landscape Ecology (IALE). His research interests include transportation planning, energy planning and Modeling, Remote Sensing, GIS, Landscape and Environmental Management. `,
   },
   {
     name: "Henry Ohiani Ohize (PhD)",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture7.png",
     bio: "Associate Professor of Electrical and Electronics Engineering at the Federal University of Technology Minna.",
-    details: "",
+    about: `Henry Ohiani Ohize is an Associate Professor of Electrical and Electronics Engineering at the Federal University of Technology Minna. Having obtained a Doctorate Degree from the University of Cape Town. With a rich passion for teaching and research, Ohize has accumulated over fifteen years of valuable experience in the domain. In the realm of research, Ohize's interests lie in the full realization and implementation of 6G technology. These encompass various aspects such as Cognitive Radio, Wireless Sensor Networks, MIMO, mmWave, and 6G application areas like e-Health, Telemedicine, and IoT, reflecting a commitment to cutting-edge technological advancements. Furthermore, Ohize has actively participated in prominent conferences, including the 2017 IEEE Wireless Communications and Networking Conference (WCNC) in the USA and the 2016 Annual IEEE International Symposium on Personal, Indoor, and Mobile Radio Communications (PIMRC) in Valencia, Spain. Ohize's contributions have been featured in several publications in Scopus indexed journals, highlighting a significant research footprint in the academic community. Notably, Ohize has received recognition for scholarly contributions, including the SA NRF awards in South Africa and the TETFUND NRF award from the Nigerian government.`,
   },
   {
     name: "Chibueze Valentine Ikpo (PhD)",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture8.png",
     bio: "Lecturer in Software Engineering at Veritas University, Nigeria.",
-    details: "",
+    about: `Dr. Chibueze Valentine Ikpo is a seasoned academic and engineer specializing in computer/software engineering, control systems, cybersecurity, and optimization. He holds a Ph.D. in Computer Engineering from Kwame Nkrumah University of Science and Technology, Ghana, an M.Sc. in Control Engineering from ABU Zaria, and a B.Eng. in Electrical/Electronic Engineering from FUT Yola. Dr. Ikpo is currently a Lecturer in Software Engineering at Veritas University, Nigeria, and previously served as Principal Engineer at Nigeria's Ministry of Communications, where he led projects in signal processing, spectrum management, and international telecommunications. His research emphasizes metaheuristic optimization, machine learning, and control theory, with numerous publications. A COREN-certified engineer, he is also a member of the Nigerian Society of Engineers and an ITU Desk Officer for Nigeria.`,
   },
   {
     name: "Taliha Abiodun Folorunso (PhD)",
-    role: "Founder & CEO",
+    role: "Co-investigator",
     image: "/assets/images/profiles/Picture9.png",
     bio: "Senior Lecturer at Mechatronics Engineering. Team lead at Advanced Engineering Innovation Research Group.",
-    details: "",
+    about: ``,
   },
 ]
