cbee6b0b89c7a4f7ff634ce9cdd4aa820c1fea0d
AbdullahAjayi <hafizajayi2308@gmail.com>
2024-11-26 08:54:51 +0100

    added new project partner

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -8,6 +8,12 @@ const Partners = () => {
             <p className="text-center text-gray-600">We are proud to collaborate with a diverse range of partners who share our vision and values</p>
             <ul className="flex flex-col md:flex-row justify-center items-center gap-20 md:gap-12 mx-auto my-20">
                 <li className="cursor-pointer">
+                    <div className="w-32 h-28 flex items-center">
+                        <Image className="w-32 h-auto  mx-auto rounded-xl" width={200} height={1} alt="partner1" src='/assets/images/partners/tetfund.jpg' />
+                    </div>
+                    <p className="text-center max-w-40 text-gray-500 text-xs mt-4">TERTIARY EDUCATION TRUST FUND (TETFund) </p>
+                </li>
+                <li className="cursor-pointer">
                     <Image className="w-32 h-32  mx-auto rounded-xl" width={200} height={1} alt="partner1" src='/assets/images/partners/Picture1.png' />
                     <p className="text-center max-w-40 text-gray-500 text-xs mt-4">Federal University of Technology, Minna, Niger State. </p>
                 </li>
